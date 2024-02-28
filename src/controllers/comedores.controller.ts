bb838bf6715ae3d98d70269ee514dbcf848a50e2
amsraimar <amsraimar@gmail.com>
2024-02-28 17:56:44 -0300

    Melhorias CRUD comedores

--- src/controllers/comedores.controller.ts
+++ src/controllers/comedores.controller.ts
@@ -1,5 +1,15 @@
+import { UpdateComerdoresDto } from 'src/dtos/update-comedores.dto';
 import { ComedoresService } from './../services/comedores.service';
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { InsertComerdoresDto } from 'src/dtos/insert-comedores.dto';
 
 @Controller({
   path: 'comedores',
@@ -10,5 +20,28 @@ export class ComedoresController {
   @Get()
   getAll() {
     return this.comedoresService.findAll();
+  }
+
+  @Get(':id')
+  getId(@Param('id') id: string) {
+    return this.comedoresService.findById(id);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.comedoresService.delete(id);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateComerdoresDto: UpdateComerdoresDto,
+  ) {
+    return this.comedoresService.update(id, updateComerdoresDto);
+  }
+
+  @Post()
+  create(@Body() insertComerdoresDto: InsertComerdoresDto) {
+    return this.comedoresService.create(insertComerdoresDto);
   }
 }
--- src/services/comedores.service.ts
+++ src/services/comedores.service.ts
@@ -2,6 +2,8 @@ import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Comedores } from 'src/schemas/comedores.schema';
+import { UpdateComerdoresDto } from 'src/dtos/update-comedores.dto';
+import { InsertComerdoresDto } from 'src/dtos/insert-comedores.dto';
 
 @Injectable()
 export class ComedoresService {
@@ -9,12 +11,30 @@ export class ComedoresService {
     @InjectModel(Comedores.name) private ComedoresModel: Model<Comedores>,
   ) {}
 
-  async create(nome: string): Promise<Comedores> {
-    const createdCat = new this.ComedoresModel({ nome: nome });
+  async create(insertComerdoresDto: InsertComerdoresDto): Promise<Comedores> {
+    const createdCat = new this.ComedoresModel(insertComerdoresDto);
     return createdCat.save();
+  }
+
+  async findById(id: string): Promise<Comedores> {
+    return this.ComedoresModel.findById(id).exec();
   }
 
   async findAll(): Promise<Comedores[]> {
     return this.ComedoresModel.find().exec();
+  }
+
+  async delete(id: string): Promise<any> {
+    return this.ComedoresModel.deleteOne({ _id: id }).exec();
+  }
+
+  async update(
+    id: string,
+    updateComerdoresDto: UpdateComerdoresDto,
+  ): Promise<any> {
+    return this.ComedoresModel.findByIdAndUpdate(
+      { _id: id },
+      updateComerdoresDto,
+    ).exec();
   }
 }

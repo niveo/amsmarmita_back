d63102e789418719868fe6f925f64ad31954479b
amsraimar <amsraimar@gmail.com>
2024-02-29 16:11:25 -0300

    Adicionado cors e helmet

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,45 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+import './common/string.extensions';
+import { HttpExceptionFilter } from './common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  app.enableCors({
+    origin: [
+      'http://localhost:4200',
+      'http://192.168.0.129:4200',
+      'https://amsmarmita-front.vercel.app',
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: [
+      'authorization',
+      'Content-Type',
+      'userid',
+      'Access-Control-Allow-Credentials',
+      'Access-Control-Allow-Origin',
+    ],
+    maxAge: 86400,
+  });
+  app.use(
+    helmet({
+      hsts: { maxAge: 31536000 },
+      frameguard: { action: 'deny' },
+      contentSecurityPolicy: {
+        directives: {
+          'default-src': ["'self'"],
+          'frame-ancestors': ["'none'"],
+        },
+      },
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();

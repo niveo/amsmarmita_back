9f385e527bb41ea74e8ce197a3b30628ce1ea7da
amsraimar <amsraimar@gmail.com>
2024-03-25 12:30:10 -0300

    Teste marmita dto

--- src/dtos/insert-marmita.dto.ts
+++ src/dtos/insert-marmita.dto.ts
@@ -1,4 +1,18 @@
+import {
+  IsDate,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Length,
+} from 'class-validator';
+
 export class InsertMarmitaDto {
+  @IsDate()
+  @IsNotEmpty()
   lancamento: Date;
+
+  @IsString()
+  @IsOptional()
+  @Length(0, 100)
   observacao: string;
 }

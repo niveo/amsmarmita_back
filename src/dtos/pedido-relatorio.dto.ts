d3d9cf716acd54342c9180ceb99b4437baaed1f8
amsraimar <amsraimar@gmail.com>
2024-04-08 10:54:48 -0300

    Pedido relatorio reformulado

--- src/dtos/pedido-relatorio.dto.ts
+++ src/dtos/pedido-relatorio.dto.ts
@@ -2,7 +2,6 @@ export class PedidoRelatorioDto {
   prato: string;
   quantidade: number;
   comedoresMap?: Map<string, PedidoRelatorioComedorDto>;
-  principal? = false;
   comedores(): PedidoRelatorioComedorDto[] {
     return [...this.comedoresMap.values()];
   }
@@ -11,5 +10,12 @@ export class PedidoRelatorioDto {
 export class PedidoRelatorioComedorDto {
   comedor: string;
   quantidade: number;
-  de?: string[];
+  de?: {
+    nome: string;
+    grupo: {
+      nome: string;
+      multiplo: boolean;
+      cor: string;
+    };
+  }[];
 }

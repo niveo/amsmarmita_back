96f4b24a98978b168b4b135ee3906403e74f0bdb
amsraimar <amsraimar@gmail.com>
2024-04-16 18:03:25 -0300

    Teste pdf

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import * as PDFKit from 'pdfkit';
+import { createWriteStream } from 'fs';
+import { join } from 'path';
+import { v4 } from 'uuid';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  createPdf() {
+    const fileName = v4() + '.pdf';
+    const pd = new PDFKit();
+    pd.fontSize(13).fillColor('#6155a4').text('Texto formatado', {
+      align: 'center',
+    });
+
+    pd.fontSize(16).fillColor('#6155a4').text('Texto formatado', {
+      align: 'center',
+    });
+
+    pd.fontSize(20).fillColor('#6155a4').text('Texto formatado', {
+      align: 'center',
+    });
+    pd.pipe(createWriteStream(fileName));
+    pd.end();
+
+    return join(process.cwd(), fileName);
   }
 }
